6555d6d0d42e1d55fcaf8619aa511f03640257cc
Brenda Azeredo <brendaazeredo@Brendas-MacBook-Air.local>
2023-10-16 11:14:44 -0300

    It is working.

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,18 +11,41 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 const WeatherApp = () => {
+    let api_key = "dd94f859a0e52d6e4767fddf735f04a7";
+
+    const search = async () => {
+        const elements = document.getElementsByClassName("cityInput");
+        if (elements[0].value === "") {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${elements[0].value}&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+
+        const humidity = document.getElementsByClassName("humidity-percent");
+        const wind = document.getElementsByClassName("wind-rate");
+        const temperature = document.getElementsByClassName("weather-temp");
+        const location = document.getElementsByClassName("weather-location");
+
+        humidity[0].innerHTML = data.main.humidity+"%";
+        wind[0].innerHTML = data.wind.speed+" km/h";
+        temperature[0].innerHTML = data.main.temp+"°C";
+        location[0].innerHTML = data.name;
+    }
+
     return (
         <div className='container'>
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder='Search'/>
-                <div className="search-icon">
+                <div className="search-icon" onClick={() => {search()}}>
                     <img src={search_icon} alt="" />
                 </div>
             </div>
             <div className="weather-image">
                 <img src={cloud_icon} alt="" />
             </div>
-            <div className="weather-temp">24°c</div>
+            <div className="weather-temp">24°C</div>
             <div className="weather-location">London</div>
             <div className="data-container">
                 <div className="element">
@@ -35,7 +58,7 @@ const WeatherApp = () => {
                 <div className="element">
                     <img src={wind_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">18km/h</div>
+                        <div className="wind-rate">18km/h</div>
                         <div className="text">Wind Speed</div>
                     </div>
                 </div>
